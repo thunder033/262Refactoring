104927d0a66bc7daea2b28774b92c1d5a9a191a3
Gregory Rozmarynowycz (RIT Student) <gjr8050@ad.rit.edu>
2016-05-01 16:49:56 -0400

    start frame testing

---
+++ test/Main/Frame/FrameTest.java
@@ -0,0 +1,87 @@
+package Main.Frame;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by gjr8050 on 5/1/2016.
+ */
+public class FrameTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void isStrike() throws Exception {
+
+    }
+
+    @Test
+    public void isSpare() throws Exception {
+
+    }
+
+    @Test
+    public void getPinCount() throws Exception {
+
+    }
+
+    @Test
+    public void getBaseScore() throws Exception {
+
+    }
+
+    @Test
+    public void isBowled() throws Exception {
+
+    }
+
+    @Test
+    public void isBowled1() throws Exception {
+
+    }
+
+    @Test
+    public void getChainScore() throws Exception {
+
+    }
+
+    @Test
+    public void setPinCount() throws Exception {
+
+    }
+
+    @Test
+    public void setFrameState() throws Exception {
+
+    }
+
+    @Test
+    public void setPinCount1() throws Exception {
+
+    }
+
+    @Test
+    public void calculateFrameScore() throws Exception {
+
+    }
+
+    @Test
+    public void isLast() throws Exception {
+
+    }
+
+    @Test
+    public void calculateTotalScore() throws Exception {
+
+    }
+
+    @Test
+    public void getTotalScore() throws Exception {
+
+    }
+}
